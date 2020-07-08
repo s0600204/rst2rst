6a1180d931fc273abe7d46adb0c026962477af54
Matěj Cepl <mcepl@cepl.eu>
2020-07-08 14:14:45 +0200

    Add missing relative imports

--- rst2rst/__init__.py
+++ rst2rst/__init__.py
@@ -1,7 +1,7 @@
 """rst2rst package."""
-from utils import LazyString, read_version
-from writer import Writer  # Required for docutils.core.publish_cmdline() to
-                           # find the writer matching "rst2rst".
+from .utils import LazyString, read_version
+from .writer import Writer  # Required for docutils.core.publish_cmdline() to
+                            # find the writer matching "rst2rst".
 
 
 __version__ = LazyString(read_version)
