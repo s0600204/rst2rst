ad424100d154a779109b1a67bb1f73418dcc484c
s0600204 <dev@s0600204>
2022-09-23 14:33:57 +0100

    Update HISTORY and version.txt

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ NAME = 'rst2rst'
 README = read_relative_file('README.rst')
 VERSION = read_relative_file(os.path.join(NAME, 'version.txt')).strip()
 PACKAGES = [NAME]
-REQUIRES = ['setuptools', 'docutils>=0.9']
+REQUIRES = ['setuptools', 'docutils>=0.10']
 ENTRY_POINTS = {
     'console_scripts': [
         'rst2rst = rst2rst.scripts.rst2rst:main',
@@ -29,7 +29,13 @@ if __name__ == '__main__':  # Don't run setup() when we import this module.
           long_description=README,
           classifiers=['Development Status :: 3 - Alpha',
                        'License :: OSI Approved :: BSD License',
-                       'Programming Language :: Python :: 2.7',
+                       'Programming Language :: Python :: 3',
+                       'Programming Language :: Python :: 3.5',
+                       'Programming Language :: Python :: 3.6',
+                       'Programming Language :: Python :: 3.7',
+                       'Programming Language :: Python :: 3.8',
+                       'Programming Language :: Python :: 3.9',
+                       'Programming Language :: Python :: 3.10',
                        'Topic :: Documentation',
                        'Topic :: Software Development :: Documentation',
                        'Topic :: Software Development :: Quality Assurance',
@@ -43,5 +49,6 @@ if __name__ == '__main__':  # Don't run setup() when we import this module.
           packages=[NAME],
           include_package_data=True,
           zip_safe=False,
+          python_requires='>=3.5.4',
           install_requires=REQUIRES,
           entry_points=ENTRY_POINTS)
